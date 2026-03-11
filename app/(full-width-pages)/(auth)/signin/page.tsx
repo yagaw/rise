b39aad09f7ed397e4ab9858ee82f66ca3ecf0129
yagaw <leyagaw@gmail.com>
2026-03-11 17:12:58 +0700

    feat(auth): refactor SignIn component to use Suspense and improve code structure; add proxy middleware for session management

--- app/(full-width-pages)/(auth)/signin/page.tsx
+++ app/(full-width-pages)/(auth)/signin/page.tsx
@@ -1,11 +1,16 @@
-import SignInForm from "@/components/auth/SignInForm";
-import { Metadata } from "next";
+import SignInForm from "@/components/auth/SignInForm"
+import { Metadata } from "next"
+import { Suspense } from "react"
 
 export const metadata: Metadata = {
   title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
   description: "This is Next.js Signin Page TailAdmin Dashboard Template",
-};
+}
 
 export default function SignIn() {
-  return <SignInForm />;
+  return (
+    <Suspense>
+      <SignInForm />
+    </Suspense>
+  )
 }
